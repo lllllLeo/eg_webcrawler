bdb32a6800b7270600fda61f1a6ff38aa7860a09
lllllLeo <andwithyj@gmail.com>
2020-11-15 14:39:37 +0900

    complete extract teacher's shedule

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium.webdriver.common.by import By
 
-with open('config.json','r') as f:
+with open('config.json', 'r') as f:
     config = json.load(f)
 
 eg_id = config['ENGOO']['ID']
@@ -18,7 +18,7 @@ password = '//*[@id="label-1"]'
 signin = '.css-16clkoc'
 favorite_teacher = '#main > div.dashboard-container > aside > div.db-sidebar > ul.list-style-none.pd-none.db-sidebar-nav > li:nth-child(4) > a'
 rena = '#content > ul > li.teacher-favorite-box.teacher-card.teacher-box-39268 > a > div.teacher-card-teacher-info > p.teacher-card-teacher-name'
-driver = webdriver.Chrome() # 같은 폴더 아니면 ()안에 경로 넣음
+driver = webdriver.Chrome()  # 같은 폴더 아니면 ()안에 경로 넣음
 # Leina https://engoo.co.kr/tutors/39572
 url = login
 driver.get(url)
@@ -27,10 +27,10 @@ driver.find_element_by_css_selector(id).send_keys(eg_id)
 driver.find_element_by_xpath(password).send_keys(eg_password)
 driver.find_element_by_css_selector(signin).click()
 driver.find_element_by_css_selector(favorite_teacher).click()
-#content > ul > li.teacher-favorite-box.teacher-card.teacher-box-39268 > a > div.teacher-card-teacher-info > p.teacher-card-teacher-name
+# content > ul > li.teacher-favorite-box.teacher-card.teacher-box-39268 > a > div.teacher-card-teacher-info > p.teacher-card-teacher-name
 # ii = driver.find_elements_by_css_selector('#content > ul > li.teacher-favorite-box.teacher-card.teacher-box-39268 > a > div.teacher-card-teacher-info > p.teacher-card-teacher-name').get_attribute("innerHTML")
 fav_teachers = []
-fav_teachers = driver.find_elements_by_tag_name('p.teacher-card-teacher-name') # 즐겨찾는 선생님 수 카운트
+fav_teachers = driver.find_elements_by_tag_name('p.teacher-card-teacher-name')  # 즐겨찾는 선생님 수 카운트
 
 # teacher_uid = driver.find_element_by_css_selector('#content > ul > li').get_attribute("class")
 # teacher_uid[-5:]
@@ -40,36 +40,59 @@ fav_teachers = driver.find_elements_by_tag_name('p.teacher-card-teacher-name') #
 # //*[@id="content"]/ul/li[4]/a/div[2]/p[1]
 # //*[@id="content"]/ul/li[14]/a/div[2]/p[1]
 
-#teacher_39572 > div:nth-child(1) > ul > li.t-10\:00
-#teacher_39572 > div:nth-child(1) > ul > li.t-10\:30
-#teacher_39572 > div:nth-child(1) > ul > li.t-10\:30
-#teacher_39572 > div:nth-child(1) > ul > li.t-11\:00
+
 for count in range(1, len(fav_teachers)):
-    teacher = driver.find_element_by_xpath('//*[@id="content"]/ul/li[' + str(count) + ']/a/div[2]/p[1]').get_attribute('innerHTML')
+    teacher = driver.find_element_by_xpath('//*[@id="content"]/ul/li[' + str(count) + ']/a/div[2]/p[1]').get_attribute(
+        'innerHTML')
     if (teacher == 'Leina'):
         # time_count = []
-        driver.find_element_by_xpath('//*[@id="content"]/ul/li[' + str(count) + ']/a/div[2]/p[1]').click() # 선생님 클릭
-        teacher_uid = driver.find_element_by_css_selector('#favorite-link > a').get_attribute("data-tutor-id") # 선생님 uid 추출
-        time_count = driver.find_element_by_css_selector('#teacher_'+str(teacher_uid)+' > div:nth-child(1) > ul > li').get_attribute('innerHTML')
-        print(time_count)
-        # for day in range(1, 7):
-        #     hh = driver.find_element_by_xpath('//*[@id="teacher_' + str(teacher_uid) +'"]/div[' + str(day) + ']')
-        #     for time in
-        #     hh.find_element_by_css_selector('a.lessons label label-info').get_attribute("innerHTML")
-        #     print(hh)
+        driver.find_element_by_xpath('//*[@id="content"]/ul/li[' + str(count) + ']/a/div[2]/p[1]').click()  # 선생님 클릭
+        break
 
+teacher_uid = driver.find_element_by_css_selector('#favorite-link > a').get_attribute("data-tutor-id")  # 선생님 uid 추출
+#   // *[ @ id = "teacher_39572"] / div[1] / ul / li[22]  10:00부터
+#   // *[ @ id = "teacher_39572"] / div[1] / ul / li[49]  23:30까지
+# for day in range(1, 7):
+# driver.find_element_by_xpath('//*[@id="teacher_' + str(teacher_uid) +'"]/div[' + str(day) + ']')
+# for time in range(22,49):
+# driver.find_elements_by_css_selector('a.lessons label label-info')
+reservation_count = []
+reservation_count = driver.find_elements_by_css_selector('a.lessons.label.label-info')
 
-        #     .find_elements_by_css_selector('a.lessons label label-info').get_attribute("innerHTML")
-        # driver.find_element_by_css_selector()
-        # for j in range(1, )
-        # driver.
+print('Leina선생님 예약 가능 수 : %s' % len(reservation_count))
 
-#dt_2020-11-17_10-30-00 > a
+i = 0
+for i in range(i, len(reservation_count)):
+    a = []
+    # a = driver.find_elements_by_xpath('//a/parent::li[@id]').get_attribute('id')
+    a = driver.find_elements_by_xpath('//a/parent::li[@id]')
+    # print(i)
+    print(a[i].get_attribute('id'))
+
+    # print(a[i].find_element_by_xpath('//a/parent::li[@id]').get_attribute('id'))
+
+    # print(i.get_attribute('innerHTML'))
+    # // *[ @ id = "dt_2020-11-17_10-30-00"]
+    # // *[ @ id = "dt_2020-11-17_10-00-00"] xpath
+    # #dt_2020-11-17_10-00-00 selector
+    # //*[@id="dt_2020-11-17_10-00-00"]/div 예약됨 xpath
+    # #dt_2020-11-17_10-00-00 > div 예약됨 셀렉터
+    #  # dt_2020-11-17_10-30-00 > a 예약하기 셀렉터
+    #  // *[ @ id = "dt_2020-11-17_10-30-00"] / a  예약하기 xpath
+
+    #     .find_elements_by_css_selector('a.lessons label label-info').get_attribute("innerHTML")
+    # driver.find_element_by_css_selector()
+    # for j in range(1, )
+    # driver.
+
+# dt_2020-11-17_10-30-00 > a
 # //*[@id="teacher_39572"]/div[1]  #teacher_39572 > div:nth-child(1)
 # //*[@id="teacher_39572"]/div[2]
-
 
 
 # 즐겨찾기 선생님에서 uid뺴오기
 # teacher_uid = driver.find_element_by_css_selector('#content > ul > li').get_attribute("class")
 #         teacher_uid = teacher_uid[-5:]
+
+# time_count = driver.find_element_by_xpath('// *[ @ id = "teacher_'+str(teacher_uid)+'"] / div[1] / ul / li[22]').get_attribute('innerHTML')
+# print(time_count) # 10:00
