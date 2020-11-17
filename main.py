6a13488fdd4d5e9a6e694cbdd768840941725ebd
lllllLeo <andwithyj@gmail.com>
2020-11-18 02:06:31 +0900

    ''->"

--- main.py
+++ main.py
@@ -20,20 +20,20 @@ import os
 #     config = json.load(f)
 
 # 선생님 이름 설정
-teacher_name = ''
+teacher_name = 'Rena'
 # 텔레그램 봇 설정
 bot_token = ''
 bot_id = ''
-bot = ''
+bot = telegram.Bot(token=bot_token)
 # 아이디, 비번 설정
 eg_id = ''
-eg_password = ''
+eg_password = '!'
 eg_login_url = 'https://engoo.co.kr/app/login?automatic=true&redirectTo=%2Fapp%2Foauth%2Fauthorize%3Fclient_id%3Dd2ac7019db25c328dc5d06b1c3c57b7ce1dcb4655d4a2a3c87b7c196875093c7%26display%3D%26intent%3Dlogin%26redirect_uri%3Dhttps%253A%252F%252Fengoo.co.kr%252Fmembers%252Fauth%252Fapp_engoo%252Fcallback%26response_type%3Dcode'
 
 id = '.css-cgadzw'
 password = '//*[@id="label-1"]'
 signin = '.css-16clkoc'
-favorite_teacher = '#main > div.dashboard-container > aside > div.db-sidebar > ul.list-style-none.pd-none.db-sidebar-nav > li:nth-child(4) > a'
+# favorite_teacher = '#main > div.dashboard-container > aside > div.db-sidebar > ul.list-style-none.pd-none.db-sidebar-nav > li:nth-child(4) > a'
 
 # chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('headless')
@@ -56,20 +56,22 @@ def job():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no--sandbox")
-    chrome_options.add_argument("--single-process")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--no--sandbox")
+    # chrome_options.add_argument("--single-process")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver = webdriver.Chrome()
     driver.get(eg_login_url)
     driver.implicitly_wait(2)
     driver.find_element_by_css_selector(id).send_keys(eg_id)
     driver.find_element_by_xpath(password).send_keys(eg_password)
     driver.find_element_by_css_selector(signin).click()
     print("==================================favorite teacher 전 들어옴")
-    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CSS_SELECTOR,"#main > div.dashboard-container > aside > div.db-sidebar > ul.list-style-none.pd-none.db-sidebar-nav > li:nth-child(4) > a")))
-    driver.find_element_by_css_selector(favorite_teacher).click()
+    # WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CSS_SELECTOR,"#main > div.dashboard-container > aside > div.db-sidebar > ul.list-style-none.pd-none.db-sidebar-nav > li:nth-child(4) > a")))
+    driver.find_element_by_css_selector("#main > div.dashboard-container > aside > div.db-sidebar > ul.list-style-none.pd-none.db-sidebar-nav > li:nth-child(4) > a").click()
+    print("제발")
     fav_teachers = []
     fav_teachers = driver.find_elements_by_tag_name('p.teacher-card-teacher-name')  # 즐겨찾는 선생님 수 카운트
     for count in range(1, len(fav_teachers)):
