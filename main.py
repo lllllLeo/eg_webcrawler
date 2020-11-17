d472557f843ad82d50a0d7434de182504a1fbb24
lllllLeo <andwithyj@gmail.com>
2020-11-17 20:56:54 +0900

    add config var HEROKU

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import telegram
 import schedule
 import time
+import os
 # import sys, os, time
 
 # exe 파일 만들 때
@@ -41,8 +42,8 @@ favorite_teacher = '#main > div.dashboard-container > aside > div.db-sidebar > u
 
 
 def job():
-    GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+    # GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
+    # CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
     now = time.localtime()
     current = "%04d-%02d-%02d %02d:%02d:%02d" % (
         now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
@@ -50,10 +51,11 @@ def job():
     print("============================ " + teacher_name + " 선생님 시간표 검색중")
 
     chrome_options = webdriver.ChromeOptions
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument('--disable-extensions')
-
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)  # 같은 폴더 아니면 ()안에 경로 넣음
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--no--sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)  # 같은 폴더 아니면 ()안에 경로 넣음
     driver.get(eg_login_url)
     driver.implicitly_wait(2)
     driver.find_element_by_css_selector(id).send_keys(eg_id)
