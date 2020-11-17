870b6f54f2903c218c7a77cc3be02a571ea0bb13
lllllLeo <andwithyj@gmail.com>
2020-11-18 00:04:04 +0900

    error disable gpu  HEROKU

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def job():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no--sandbox")
     chrome_options.add_argument("--disable-dev-sh-usage")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
