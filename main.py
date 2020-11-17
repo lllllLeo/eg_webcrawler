dea1002bbb62c257868de674067002161ad9559f
lllllLeo <andwithyj@gmail.com>
2020-11-17 21:17:36 +0900

    add option --disable-gpu  HEROKU

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def job():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no--sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)  # 같은 폴더 아니면 ()안에 경로 넣음
     driver.get(eg_login_url)
