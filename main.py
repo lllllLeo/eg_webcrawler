acb57e5e82b7eb6a067b4a502e018c2d07117925
lllllLeo <andwithyj@gmail.com>
2020-11-17 22:36:44 +0900

    --headless 삭제

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def job():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no--sandbox")
     chrome_options.add_argument("--single-process")
