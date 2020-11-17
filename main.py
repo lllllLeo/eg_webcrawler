dc1c45c1979adccbf2a015f80e197365f12afd05
lllllLeo <andwithyj@gmail.com>
2020-11-17 21:31:32 +0900

    add argument single process HEROKU

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def job():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no--sandbox")
+    chrome_options.add_argument("--single-process")
     chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(eg_login_url)
