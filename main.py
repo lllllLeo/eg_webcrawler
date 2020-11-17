4e184679b04f37f0f2f0c5b4348e84bb7825be25
lllllLeo <andwithyj@gmail.com>
2020-11-17 21:42:29 +0900

    GOOGLE_CHROME_BIN, CHROMEDRIVER_PATH 2 HEROKU

--- main.py
+++ main.py
@@ -51,13 +51,13 @@ def job():
     print("============================ " + teacher_name + " 선생님 시간표 검색중")
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get(GOOGLE_CHROME_BIN)
+    chrome_options.binary_location = GOOGLE_CHROME_BIN
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no--sandbox")
     chrome_options.add_argument("--single-process")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(executable_path=os.environ.get(CHROMEDRIVER_PATH), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get(eg_login_url)
     driver.implicitly_wait(2)
     driver.find_element_by_css_selector(id).send_keys(eg_id)
