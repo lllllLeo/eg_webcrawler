01d39fe0bbf2bb5d47c178c9a8ba2f33651dd394
lllllLeo <andwithyj@gmail.com>
2020-11-17 23:01:51 +0900

    disable dev shm usage 삭제

--- main.py
+++ main.py
@@ -52,11 +52,11 @@ def job():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = GOOGLE_CHROME_BIN
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no--sandbox")
     chrome_options.add_argument("--single-process")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get(eg_login_url)
     driver.implicitly_wait(2)
